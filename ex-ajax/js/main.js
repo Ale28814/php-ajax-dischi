72305f7def97e552329197fd49ff8d891d48e08a
Ale28814 <alened928@gmail.com>
2021-12-31 13:28:12 +0100

    first commit

---
+++ ex-ajax/js/main.js
@@ -0,0 +1,21 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        albums: null,
+    },
+    created() {
+        this.getAlbums()
+    },
+    methods: {
+        getAlbums() {
+            axios
+                .get('http://localhost/php-ajax-dischi/ex-ajax/scripts/database.php')
+                .then(response => {
+                    this.albums = response.data;
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        },
+    },
+});
